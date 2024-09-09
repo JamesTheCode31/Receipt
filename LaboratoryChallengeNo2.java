c0a2a57572ef8e04e5911fa73c0e7d90cb316a30
JamesTheCode31 <jsn8340@students.uc-bcf.edu.ph>
2024-09-09 11:50:15 +0800

    Add files via upload

---
+++ LaboratoryChallengeNo2.java
@@ -0,0 +1,17 @@
+public class LaboratoryChallengeNo2 {
+	    public static void main(String[] args) {
+	        // Given values
+	        double length = 27.0; // meters
+	        double width = 10.0; // meters
+
+	        // Calculations
+	        double area = length * width;
+	        double perimeter = 2 * (length + width);
+
+	        // Output
+	        System.out.println("Length: " + length + " meters");
+	        System.out.println("Width: " + width + " meters");
+	        System.out.println("Area: " + area + " square meters");
+	        System.out.println("Perimeter: " + perimeter + " meters");
+	    }
+	}
